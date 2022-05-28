599ead2d046ed97242c8e5ef38be501ed5634008
chenweicong <chenweicong@chexiang.com>
2022-05-28 19:35:47 +0800

    切换github代理地址

--- entry.js
+++ entry.js
@@ -1,1 +1,5 @@
-eval(http.get('https://raw.fastgit.org/touchren/hemaqiangcai/main/hemaqiangcai.js').body.string());
+eval(http.get('https://js.dayplus.xyz/https://raw.githubusercontent.com/touchren/hemaqiangcai/0506/hemaqiangcai.js').body.string());
+// https://raw.githubusercontent.com/touchren/hemaqiangcai/0506/hemaqiangcai.js
+// https://raw.fastgit.org/touchren/hemaqiangcai/0506/hemaqiangcai.js
+// https://gpproxy.com/https://raw.githubusercontent.com/touchren/hemaqiangcai/0506/hemaqiangcai.js
+// https://js.dayplus.xyz/https://raw.githubusercontent.com/touchren/hemaqiangcai/0506/hemaqiangcai.js
